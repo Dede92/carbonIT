39da834c62857be39cfa3a3b7bba0061d713ddd6
dho <dho@quinten-france.com>
2020-11-08 16:20:08 +0100

    add function for moving adventurers depeding on state

--- main.py
+++ main.py
@@ -88,6 +88,66 @@ def init_treasure(map, input):
     for ele in input['T']:
         map[ele['y']][ele['x']] = ele['N']
 
+def move_adventurers(map, input):
+    adventurers = input['A']
+    final_adventurers = dict()
+    for adven in adventurers:
+        print(adven)
+        pos_x = adven['x']
+        pos_y = adven['y']
+        compass = ['N', 'E', 'S', 'W']
+        direc = compass.index(adven['D'])
+        num_tre = 0
+        sequences = [char for char in adven['S']]
+
+        for mvt in sequences:
+            # print('MVT: ', mvt)
+            # print('DIREC: ', direc)
+            # print('POS_X: {}, POS_Y:{}'.format(pos_x, pos_y))
+            if mvt == 'A':
+                futur_y = pos_y
+                futur_x = pos_x
+                if direc == 0:
+                    futur_y -= 1
+                if direc == 1:
+                    futur_x += 1
+                if direc == 2:
+                    futur_y += 1
+                if direc == 3:
+                    futur_x -= 1
+                
+                #  Check map
+                pos_map = map[futur_y][futur_x]
+                # print('F_X: {}, F_Y:{}'.format(futur_x, futur_y))
+                # print('POS_MAP: ', pos_map)
+                if pos_map == 'M':
+                    pass
+                elif isinstance(pos_map, int):
+                    if pos_map > 0:
+                        num_tre += 1
+                        map[futur_y][futur_x] = map[futur_y][futur_x] - 1
+                    pos_x = futur_x
+                    pos_y = futur_y
+                else: 
+                    pos_x = futur_x
+                    pos_y = futur_y
+            if mvt == 'D':
+                direc += 1
+                if direc > 3:
+                    direc = 0
+            if mvt == 'G':
+                direc -= 1
+                if direc < 0:
+                    direc = 3
+        final_adventurers[adven['name']] = {
+            'pos_x': pos_x,
+            'pos_y': pos_y,
+            'num_tre': num_tre,
+            'D': compass[direc],
+        }
+    print(map)
+    print(final_adventurers)
+    pass
 
 
 if __name__ == "__main__":
